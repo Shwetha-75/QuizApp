9ffa86315a2ddbf86f167b61fa9725e77195c683
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-02-22 10:25:22 +0530

    Update JavaQuestionRepository.java

--- QuizServiceApplication/src/main/java/com/quiz/mvcproject/repositories/JavaQuestionRepository.java
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/repositories/JavaQuestionRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.quiz.mvcproject.entities.Questionjava;
 
-public interface JavaQuestionRepository extends JpaRepository<Questionjava, Integer> {
+public interface JavaQuestionRepository extends JpaRepository<Questionjava, Integer> 
+{
+  public Questionjava findById(int id);
 
 }
