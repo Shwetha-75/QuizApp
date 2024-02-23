0f114d23650c8041dc976b4a54160b075d144476
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-02-23 10:19:17 +0530

    Add files via upload

--- QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/QuestionJavaController.java
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/QuestionJavaController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.quiz.mvcproject.entities.Questionjava;
 import com.quiz.mvcproject.services.JavaQuestionService;
+import com.quiz.mvcproject.services.JavaQuestionServiceImplementation;
 
 
 @Controller
@@ -20,6 +22,16 @@ public class QuestionJavaController
 {
 	@Autowired
 	JavaQuestionService javService;
+	
+	Questionjava javQuestion;
+	
+	
+     
+	//we cannot have the List<Integer> listid=javService.getIdList as instance variable
+	//since this is specific to QuestionJavaController class we are instanciating the class 
+	//any where and also i cannot have it as ststaic variable since javService reference
+	//memory will be allocated at the run time
+	
 	
 	@PostMapping("/javcreateQuestion")
 	public String addQuestions(@ModelAttribute Questionjava jav) 
@@ -32,13 +44,25 @@ public class QuestionJavaController
 	@PostMapping("/javremoveQuestion")
 	public String removeQuestion(@RequestParam int id) 
 	{
-		String msg=javService.removeQuestion(id);
-		return msg;
+		//getIdList lists the id;s avaliable in table and 
+		//returns all ths ids 
+		//we have to check for if the id exists in the list or not
+		if(javService.getIdList().contains(id))
+		{
+			String msg=javService.removeQuestion(id);
+			return msg;
+		}
+		else
+		{
+			return "iddoesnotexists";
+		}
+		
 	}
 
 	@PostMapping("/javupdateQuestion")
 	public String updateQuestion(@ModelAttribute Questionjava jav) 
 	{	
+		
 		String msg=javService.updateQuestion(jav);
 		return msg;
 	}
@@ -51,6 +75,7 @@ public class QuestionJavaController
 		model.addAttribute("quizQuestions",list);
 		return "viewAll";
 	}
+	
 	@GetMapping("/map-javpaper")
 	public String javPaper(Model model)
 	{
@@ -60,6 +85,30 @@ public class QuestionJavaController
 		return "javquestionpaper";
 		
 	}
+	
+	
+	//receive a request parameter as id 
+	//using this id we will create a model 
+	//ans display that to the update form
+	@PostMapping("/map-updatejavaid")
+	public String updateQuestionDisplayModel(@RequestParam int id, Model model)
+	{
+		//getIdList lists the id;s avaliable in table and 
+				//returns all ths ids 
+				//we have to check for if the id exists in the list or not
+		if(javService.getIdList().contains(id))
+		{
+		    Questionjava javObject=javService.findByIdObject(id);
+		    model.addAttribute("javObject", javObject);
+		
+		    return "javupdateQuestion";
+		}
+		else
+		{
+			return "iddoesnotexists";
+		}
+	}
+	
 
 
 }
