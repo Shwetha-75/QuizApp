1d8ced80aa4e3fb8d616d3d6b71d9c22db142cc5
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-02-23 10:19:50 +0530

    Add files via upload

--- QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/SQLQuestionController.java
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/SQLQuestionController.java
@@ -10,45 +10,63 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import com.quiz.mvcproject.entities.Questionsql;
 
 import com.quiz.mvcproject.services.SQLQuestionService;
 
 @Controller
-public class SQLQuestionController 
-{
+public class SQLQuestionController {
 	@Autowired
 	SQLQuestionService sqlService;
-	
+
 	@PostMapping("/sqcreateQuestion")
-	public String addQuestions(@ModelAttribute Questionsql sq) 
-	{
-		String msg=sqlService.addQuestions(sq);
+	public String addQuestions(@ModelAttribute Questionsql sq) {
+		String msg = sqlService.addQuestions(sq);
 		return msg;
 	}
 
 	@PostMapping("/sqremoveQuestion")
-	public String removeQuestion(@RequestParam int id) 
-	{
-		String msg=sqlService.removeQuestion(id);
-		return msg;
+	public String removeQuestion(@RequestParam int id) {
+		if (sqlService.getIdAll().contains(id)) 
+		{
+			String msg = sqlService.removeQuestion(id);
+			return msg;
+		} 
+		else 
+		{
+			return "iddoesnotexists";
+		}
 	}
 
 	@PostMapping("/squpdateQuestion")
-	public String updateQuestion(@ModelAttribute Questionsql sq) 
-	{	
-		String msg=sqlService.updateQuestion(sq);
+	public String updateQuestion(@ModelAttribute Questionsql sq) {
+		String msg = sqlService.updateQuestion(sq);
 		return msg;
 	}
 
 	@GetMapping("/sqfetchAll")
-	public String fetchAll(Model model) 
-	{
-		List<Questionsql> list=sqlService.fetchAll();
-		model.addAttribute("quizQuestions",list);
+	public String fetchAll(Model model) {
+		List<Questionsql> list = sqlService.fetchAll();
+		model.addAttribute("quizQuestions", list);
 		return "viewAll";
 	}
-	
+
+	@PostMapping("/map-sqlupdateid")
+	public String updateQuestionDisplayModel(@RequestParam int id, Model model) 
+	{
+		if (sqlService.getIdAll().contains(id)) 
+		{
+
+		    Questionsql sqlObj = sqlService.findByIdObject(id);
+		    // getting the object by id and preparing the model
+		    model.addAttribute("sqlObj", sqlObj);
+
+		    return "squpdateQuestion";
+		}
+		else
+		{
+			return "iddoesnotexists";
+		}
+	}
 
 }
