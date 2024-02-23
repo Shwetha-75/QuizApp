53d09546d92393aeae6b8fafc43ef83374470f2d
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-02-23 10:18:47 +0530

    Add files via upload

--- QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/PythonQuestionController.java
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/PythonQuestionController.java
@@ -23,6 +23,7 @@ public class PythonQuestionController
 	@PostMapping("/pytcreateQuestion")
 	public String addQuestions(@ModelAttribute Questionpython pyt) 
 	{
+		//creating object and saving the bean
 		String msg=pythonService.addQuestions(pyt);
 		return msg;
 	}
@@ -30,8 +31,39 @@ public class PythonQuestionController
 	@PostMapping("/pytremoveQuestion")
 	public String removeQuestion(@RequestParam int id) 
 	{
-		String msg=pythonService.removeQuestion(id);
-		return msg;
+		if(pythonService.getIdAll().contains(id))
+		{
+			String msg=pythonService.removeQuestion(id);
+			return msg;
+		}
+		else
+		{
+			return "iddoesnotexists";
+		}
+		
+	}
+	//changes applied in uodate 
+	//it should display the data before updation 
+	//admin can make tha vhanges easily
+	@PostMapping("/map-pytupdatelist")
+	public String updateQuestionDisplayModel(@RequestParam int id,Model model)
+	{
+		//findby id method will returns the object 
+		//by id 
+		//System.out.println(id);
+		
+		//we have to check whether id exists or not 
+		if(pythonService.getIdAll().contains(id))
+		{
+			Questionpython pyt=pythonService.findByIdObject((id));
+			model.addAttribute("pyt", pyt);//creating model 
+			return "pytupdateQuestion";
+		}
+		else
+		{
+			return "iddoesnotexists";
+		}
+		
 	}
 
 	@PostMapping("/pytupdateQuestion")
@@ -44,8 +76,8 @@ public class PythonQuestionController
 	@GetMapping("/pytfetchAll")
 	public String fetchAll(Model model) 
 	{
-		List<Questionpython> list=pythonService.fetchAll();
-		model.addAttribute("quizQuestions",list);
+		List<Questionpython> list=pythonService.fetchAll();//fecting all the objects 
+		model.addAttribute("quizQuestions",list);//creating the model
 		return "viewAll";
 	}
 	
