ea0a34a55cf19049cc77e124dcd298a10798841a
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-02-23 10:17:10 +0530

    Add files via upload

--- QuizServiceApplication/src/main/java/com/quiz/mvcproject/repositories/PythonQuestionRepository.java
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/repositories/PythonQuestionRepository.java
@@ -1,11 +1,20 @@
 package com.quiz.mvcproject.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.quiz.mvcproject.entities.Questionpython;
 
-public interface PythonQuestionRepository extends JpaRepository<Questionpython,Integer>
-  {
-    public Questionpython findById(int id);
+public interface PythonQuestionRepository extends JpaRepository<Questionpython,Integer> 
+{
+	
+	public Questionpython findById(int id);
+	
+	
+	
+	@Query(value="select id from questionpython", nativeQuery=true)
+	public List<Integer> findByIdsAll();
 
 }
