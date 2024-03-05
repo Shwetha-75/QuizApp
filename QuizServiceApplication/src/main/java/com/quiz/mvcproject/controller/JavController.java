29dfdf7555c50b95bcfaafb74dda7eec81b67613
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-03-05 10:32:33 +0530

    Create JavController.java

---
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/JavController.java
@@ -0,0 +1,25 @@
+package com.quiz.mvcproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.mvcproject.entities.Questionjava;
+import com.quiz.mvcproject.services.JavaQuestionService;
+
+@RestController
+public class JavController 
+{
+	@Autowired
+	JavaQuestionService javService;
+
+	
+	
+	@GetMapping("/jav-map")
+	public List<Questionjava> javPaper()
+	{
+		return javService.fetchAll();
+	}
+}
