101a4ea0c0aab2a2a2966d45b18731ae31ec0f8d
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-03-12 10:24:15 +0530

    Add files via upload

---
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/PythonController.java
@@ -0,0 +1,28 @@
+package com.quiz.mvcproject.controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.quiz.mvcproject.entities.Questionpython;
+import com.quiz.mvcproject.services.PythonQuestionService;
+
+@Controller
+public class PythonController 
+{
+	@Autowired
+	PythonQuestionService pytService;
+	
+	
+	@GetMapping("/pyt-paper")
+	public List<Questionpython> fetchAll()
+	{
+		
+//		System.out.println(pytService.fetchAll());
+	
+		return pytService.fetchAll();
+	}
+}
