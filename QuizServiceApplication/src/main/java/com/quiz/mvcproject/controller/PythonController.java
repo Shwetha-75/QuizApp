03aff29afa9ab16f9e5d2b16cafff66f6c048be8
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-04-24 06:12:56 +0530

    Add files via upload

--- QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/PythonController.java
+++ QuizServiceApplication/src/main/java/com/quiz/mvcproject/controller/PythonController.java
@@ -1,30 +1,38 @@
 package com.quiz.mvcproject.controller;
-
+import com.quiz.mvcproject.entities.Student;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.mvcproject.entities.Questionpython;
+import com.quiz.mvcproject.entities.Student;
 import com.quiz.mvcproject.services.PythonQuestionService;
 
-
-/*Make sure that it has to be rest controller */
 @RestController
 public class PythonController 
 {
 	@Autowired
 	PythonQuestionService pytService;
+	Student student;
 	
+	public PythonController(Student student)
+	{
+		this.student=student;
+		
+	}
 	
 	@GetMapping("/pyt-map")
-	public List<Questionpython> fetchAll()
+	public List<Questionpython> pytpaper(Model model)
 	{
 		
-//		System.out.println(pytService.fetchAll());
-	
+		
+		model.addAttribute("student",student);
+		
 		return pytService.fetchAll();
 	}
+	
+
 }
